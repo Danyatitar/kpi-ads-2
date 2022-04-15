c707ccd9f6542851edb235afd113f01370447946
Danyatitar <87567245+Danyatitar@users.noreply.github.com>
2022-04-15 12:08:17 +0300

    Add files via upload

---
+++ Bubble sort.py
@@ -0,0 +1,17 @@
+import random
+def sort(a=[]):
+    n = len(a)
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if (a[j] > a[j + 1]):
+                p=a[j]
+                a[j]=a[j+1]
+                a[j+1]=p
+    return a
+a=[random.randint(0,1000) for i in range(100)]
+print("unsorted array")
+print(a)
+a=sort1(a)
+print("Sorted array")
+print(a)
+
